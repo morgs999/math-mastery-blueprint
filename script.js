46fda66bff0aa8a0a9773ca0a0d650e5bc6038d5
Morgan <109176008+morgs999@users.noreply.github.com>
2024-09-12 11:37:13 -0400

    First big sample set
    Set up page to try out fonts, colors, layouts, approval from company

---
+++ script.js
@@ -0,0 +1,9 @@
+const title = document.querySelector('.title');
+const subtitle = document.querySelector('.subtitle');
+const paragraph = document.querySelector('.paragraph');
+
+window.addEventListener('scroll', () => {
+    document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+}, false);
+
+
